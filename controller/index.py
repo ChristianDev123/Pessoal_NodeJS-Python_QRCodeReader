041e524a9c894ae1a6c5626577ade537453432f4
ChristianDev123 <christian.lima999@gmail.com>
2022-09-28 22:57:40 -0300

    Reader QRCode Ready

--- controller/index.py
+++ controller/index.py
@@ -1,4 +1,13 @@
+from array import array
 from PIL import Image
+from pathlib import Path
 from pyzbar.pyzbar import decode
-textQR = decode(Image.open('../public/Image_teste_utf8.png'))
+import sys
+
+nameImage = sys.argv[1] 
+pathImage = str(Path(__file__)).split('\\')
+pathImage = pathImage[0:len(pathImage)-2]
+pathImage = "\\".join(pathImage)
+pathImage = f"{pathImage}\\public\\{nameImage}"
+textQR = decode(Image.open(pathImage))
 print(textQR[0].data.decode("UTF-8"))
--- router/index.js
+++ router/index.js
@@ -1,9 +1,27 @@
-const { Router } = require('express');
+const { Router} = require('express');
 const upload = require('../middlewares/multer');
 const route = Router();
+const path = require('path');
+const { spawn } = require('child_process');
 
 route.post('/',upload.array('qrcodes'),async (req, res)=>{
-        
+    try{
+        const qrcodes = req.files;
+        const jsons = [];
+        qrcodes.forEach((item)=>{
+            const process = spawn('python', [`index.py`, item.filename], {
+                cwd:path.resolve(__dirname, '../controller')
+            });
+            process.stdout.on('data',(response)=>jsons.push(JSON.parse(response.toString())));
+        })
+        setTimeout(()=>{
+            const codes = jsons.map(({msg})=>msg);
+            res.status(200).json({msg:'Qrcodes readed!', codes})
+        },250*qrcodes.length)
+    }catch(error){
+        if(error) console.log(error);
+        res.status(500).json({msg:"[ERROR] Can't read the qrcodes"});
+    }
 })
 
 module.exports = route;
